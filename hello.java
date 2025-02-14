90e97292f7eb29add2fca2a3159efb6dffa115a4
mounika13-7-94 <Mounikakukatla68@gmail.com>
2025-02-14 09:08:59 +0200

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,8 @@
+public class hello
+  {
+    public static void main (string args[])
+    {
+      system.out.println("welcom to devops lab");
+      system.out.println("14.02.2025,1849 pm");
+    }
+  }
